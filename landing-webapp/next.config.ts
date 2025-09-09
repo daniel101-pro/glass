1cf34bdc0d4b9683029674cbcc83078f583226b7
Daniel Falodun <falodun379@gmail.com>
2025-09-09 11:40:47 +0100

    new 101 updates

--- landing-webapp/next.config.ts
+++ landing-webapp/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  turbopack: {
+    root: "./landing-webapp"
+  }
 };
 
 export default nextConfig;
